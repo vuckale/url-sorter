d2da589491cb607e402baf821b3a2888cfc2ec9d
vuckale <56733276+vuckale@users.noreply.github.com>
2020-12-26 21:47:37 +0100

    fixed bug where some urls wouldn't be recognized as valid

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ def Convert(a):
     res_dct = dict(zip(it, it))
     return res_dct
 
-def uri_validator(x):
+def uri_validator(url):
     try:
-        result = urlparse(x)
+        if not url.endswith("/"):
+            url = url + "/"
+        result = urlparse(url)
         return all([result.scheme, result.netloc, result.path])
     except:
-        return False
+       return False
+
 
 def print_stats(source):
     for key, value in source.items():
@@ -26,13 +29,17 @@ def print_stats(source):
 usage = "usage: %prog [options]"
 parser = OptionParser(usage=usage)
 parser.add_option("-o", "--output",
-                action="store_true", dest="file_name",
+                action="store_true", dest="output",
                 help="write to files")
+parser.add_option("-s", "--stats",
+                action="store_true", dest="stats",
+                help="print stats of parsed urls")
 
 (options, args) = parser.parse_args()
 
 file = open('input.txt', 'r')
 lines = file.readlines()
+#print(lines)
 file.close
 
 # remove trailing lines
@@ -44,9 +51,11 @@ for line in lines:
 # sort
 netlocs = []
 for line in lines_w_o_trailing_newline:
+    #print(uri_validator(line))
     if uri_validator(line):
+        print(uri_validator(line))
         netloc = (urlparse(line)).netloc
-        if not (netloc == '') or not (netloc == '\n'):
+        if not (netloc == '') and not (netloc == '\n'):
             if netloc not in netlocs:
                 netlocs.append(netloc)
                 netlocs.append([])
@@ -58,12 +67,16 @@ for line in lines_w_o_trailing_newline:
                 url = (urlparse(line)).geturl()
                 (netlocs.__getitem__(netloc_idx+1)).append(url)
 
+print(netlocs)
 # cpnvert to dict
 netlocs_dict = Convert(netlocs)
 
-#print_stats(netlocs_dict)
 
-if options.file_name:
+
+if options.stats:
+    print_stats(netlocs_dict)
+
+if options.output:
 # write to separate files
     for key in netlocs_dict:
         print(key)
